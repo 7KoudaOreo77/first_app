3f86ee149290e5d3c458164b877255fc22914122
EC2 Default User <ec2-user@ip-172-31-37-35.ec2.internal>
2022-06-19 12:48:13 +0000

    [Add]f.text_field

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_06_19_101536) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
